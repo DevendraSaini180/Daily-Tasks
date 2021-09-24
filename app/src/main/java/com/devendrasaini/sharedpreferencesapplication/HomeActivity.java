f7291fdf87a0c0ad0212cb1fce06c9cda1badfbd
Devendra Saini <devensaini1@gmail.com>
2021-09-24 18:44:58 +0530

    Intial commit

--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/HomeActivity.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/HomeActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     TextView text_name, text_email, text_password;
     Button btn_logout;
@@ -20,42 +21,62 @@ public class DashBoardActivity extends AppCompatActivity {
     public static final String KEY_EMAIL = "User_Email";
 
     SharedPreferences sharedPreferences;
+    String mName, mEmail, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board);
+        setContentView(R.layout.activity_home);
 
+        uiInitialization();
+        setSharedPreferences();
+        logOut();
+    }
+
+    public void uiInitialization() {
         text_name = findViewById(R.id.textView3);
         text_email = findViewById(R.id.textView4);
         text_password = findViewById(R.id.textView5);
         btn_logout = findViewById(R.id.button2);
+    }
 
+    public void setSharedPreferences() {
         sharedPreferences = getSharedPreferences(MY_PREFERENCES, MODE_PRIVATE);
 
-        String name = sharedPreferences.getString(KEY_NAME, null);
-        String email = sharedPreferences.getString(KEY_EMAIL, null);
-        String password = sharedPreferences.getString(KEY_PASSWORD, null);
-
-        if(name != null || email != null || password != null)
+        mName = sharedPreferences.getString(KEY_NAME, null);
+        mEmail = sharedPreferences.getString(KEY_EMAIL, null);
+        mPassword = sharedPreferences.getString(KEY_PASSWORD, null);
+        if(checkIsFieldEmpty())
         {
-            text_name.setText(KEY_NAME +" -> " +  name);
-            text_email.setText(KEY_EMAIL +" -> " +  email);
-            text_password.setText(KEY_PASSWORD +" -> " +  password);
+            text_name.setText(KEY_NAME +" -> " +  mName);
+            text_email.setText(KEY_EMAIL +" -> " +  mEmail);
+            text_password.setText(KEY_PASSWORD +" -> " +  mPassword);
         }
+        else
+        {
+            Toast.makeText(this, "enter user details", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    public boolean checkIsFieldEmpty() {
+        if(TextUtils.isEmpty(mName) || TextUtils.isEmpty(mEmail) || TextUtils.isEmpty(mPassword))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void logOut() {
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.clear();
                 editor.commit();
-                Toast.makeText(DashBoardActivity.this, "Logout successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(HomeActivity.this, "Logout successfully", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
-
     }
-
 
 }
--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,31 +21,49 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY_EMAIL = "User_Email";
 
     SharedPreferences sharedPreferences;
+    String mName, mEmail, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        uiInitialization();
+        setSharedPreferences();
+        logIn();
+
+    }
+
+    public void uiInitialization() {
         textName = findViewById(R.id.editTextName);
         textPassword = findViewById(R.id.editTextPassword);
         textEmail = findViewById(R.id.editTextEmail);
         btnSave = findViewById(R.id.button);
+    }
 
+    public void setSharedPreferences() {
         sharedPreferences = getSharedPreferences(MY_PREFERENCES, MODE_PRIVATE);
 
+        mName = sharedPreferences.getString(KEY_NAME, null);
+        mEmail = sharedPreferences.getString(KEY_EMAIL, null);
+        mPassword = sharedPreferences.getString(KEY_PASSWORD, null);
 
-        //When open activity first check shared preferences data available or not
-        String name = sharedPreferences.getString(KEY_NAME, null);
-        String email = sharedPreferences.getString(KEY_EMAIL, null);
-        String password = sharedPreferences.getString(KEY_PASSWORD, null);
-        if(name != null || email != null || password != null)
+        if(checkIsFieldEmpty())
         {
-            //if data available so directly call Dashboard Activity
-            Intent intent = new Intent(MainActivity.this, DashBoardActivity.class);
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);
         }
+    }
 
+    public boolean checkIsFieldEmpty() {
+        if(TextUtils.isEmpty(mName) || TextUtils.isEmpty(mEmail) || TextUtils.isEmpty(mPassword))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void logIn() {
         btnSave.setOnClickListener(v -> {
             String nameVal = textName.getText().toString();
             String passwordVal = textPassword.getText().toString();
@@ -58,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             editor.putString(KEY_EMAIL, emailVal);
             editor.apply();
 
-            Intent intent = new Intent(MainActivity.this, DashBoardActivity.class);
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);
             Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
         });
