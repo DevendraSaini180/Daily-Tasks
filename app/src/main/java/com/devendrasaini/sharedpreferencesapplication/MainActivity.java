361d4c8bb3076cffd95fd1e6fa014319fcf42ce1
Devendra Saini <devensaini1@gmail.com>
2021-09-28 13:43:03 +0530

    Use of Picasso Image Library

--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
@@ -3,17 +3,14 @@ package com.devendrasaini.sharedpreferencesapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText, editNumber;
-    TextView primeNumber, factorial;
-    Button btnSave, btnCalculate;
+    String URL = "https://en.wikipedia.org/wiki/Wildlife#/media/File:Wildlife_at_Maasai_Mara_(Lion).jpg";
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,51 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         uiInitialization();
-        startButton();
-        startCalculationButton();
+        Picasso.with(this)
+                .load(URL)
+//                .placeholder(R.drawable.ic_launcher_foreground)
+//                .error(R.drawable.ic_launcher_background)
+                .fit()
+                .into(image);
+        //loadImageFromUrl();
     }
 
     public void uiInitialization() {
-        editText = findViewById(R.id.editText);
-        editNumber = findViewById(R.id.editTextNumber);
-        primeNumber = findViewById(R.id.textViewPrime);
-        factorial = findViewById(R.id.textViewFact);
-        btnSave = findViewById(R.id.button);
-        btnCalculate = findViewById(R.id.button2);
-    }
-    private void startButton() {
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editText.getText().toString().isEmpty())
-                {
-                    editText.setError("Enter Text");
-                }
-                else
-                {
-                    String editValue = editText.getText().toString();
-                    SingletonExample singletonexample = SingletonExample.getInstance();
-                    singletonexample.setText(editValue);
-                    Toast.makeText(MainActivity.this, singletonexample.getText(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        image = findViewById(R.id.imageView);
     }
 
-    public void startCalculationButton() {
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Helper.isPrime(Integer.parseInt(editNumber.getText().toString())))
-                {
-                    primeNumber.setText(editNumber.getText().toString()+" is a Prime Number");
-                }
-                else
-                {
-                    primeNumber.setText(editNumber.getText().toString()+" is not a Prime Number");
-                }
-                factorial.setText("factorial of " + editNumber.getText().toString()+" = " + Helper.findFactorial(Integer.parseInt(editNumber.getText().toString())));
-            }
-        });
-    }
+//    public void loadImageFromUrl() {
+//
+//    }
 }
