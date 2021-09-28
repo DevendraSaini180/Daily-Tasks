315bca2ed206a64dcdad1c6cb8ff66857592e826
Devendra Saini <devensaini1@gmail.com>
2021-09-28 16:08:10 +0530

    corrected url

--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    String URL = "https://en.wikipedia.org/wiki/Wildlife#/media/File:Wildlife_at_Maasai_Mara_(Lion).jpg";
-    ImageView image;
+    String URL = "https://images.unsplash.com/photo-1631473126856-d1fdf3bf9c4f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80";
+    ImageView image, glideImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         uiInitialization();
-        Picasso.with(this)
-                .load(URL)
-//                .placeholder(R.drawable.ic_launcher_foreground)
-//                .error(R.drawable.ic_launcher_background)
-                .fit()
-                .into(image);
-        //loadImageFromUrl();
+        loadImageUrlFromPicasso();
     }
-
     public void uiInitialization() {
         image = findViewById(R.id.imageView);
+        glideImage = findViewById(R.id.imageViewG);
     }
-
-//    public void loadImageFromUrl() {
-//
-//    }
+    public void loadImageUrlFromPicasso() {
+        Picasso.with(this)
+                .load(URL)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_background)
+                .fit()
+                .into(image);
+    }
 }
