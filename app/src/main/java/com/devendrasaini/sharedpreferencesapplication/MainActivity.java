4cec581d3af5ffec211bff95b4ace1330e02ac0f
Devendra Saini <devensaini1@gmail.com>
2021-09-29 14:17:52 +0530

    Use of Retrofit

--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/MainActivity.java
@@ -3,7 +3,7 @@ package com.devendrasaini.sharedpreferencesapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,8 +15,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    String mUrl = "https://picsum.photos/200";
-    ListView mPhotos;
+    String mUrl = "https://picsum.photos/";
+    TextView mApiResult;
     Retrofit retrofit;
 
     @Override
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         getApiInstance();
     }
     public void uiInitialization() {
-        mPhotos = findViewById(R.id.imageList);
+        mApiResult = findViewById(R.id.imageList);
     }
 
     public void retrofitBuilder() {
@@ -39,17 +39,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getApiInstance() {
-        PhotosAPICall photosAPICall = retrofit.create(PhotosAPICall.class);
+        PhotosAPIService photosAPICall = retrofit.create(PhotosAPIService.class);
         Call<List<PhotosModel>> modelCall = photosAPICall.getPhotos();
         modelCall.enqueue(new Callback<List<PhotosModel>>() {
             @Override
             public void onResponse(Call<List<PhotosModel>> call, Response<List<PhotosModel>> response) {
 
+                if(!response.isSuccessful())
+                {
+                    mApiResult.setText("Code: " + response.code());
+                }
+
+                List<PhotosModel> photos = response.body();
+                for (PhotosModel photo : photos )
+                {
+                    String content = "";
+                    content += "ID: " + photo.getId() + "\n";
+                    content += "Author: " + photo.getAuthor() + "\n";
+                    content += "Url: " + photo.getUrl() + "\n\n";
+                    mApiResult.append(content);
+                }
             }
 
             @Override
             public void onFailure(Call<List<PhotosModel>> call, Throwable t) {
-
+                mApiResult.setText(t.getMessage());
             }
         });
     }
--- app/src/main/java/com/devendrasaini/sharedpreferencesapplication/PhotosAPIService.java
+++ app/src/main/java/com/devendrasaini/sharedpreferencesapplication/PhotosAPIService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface PhotosAPICall {
+public interface PhotosAPIService {
     @GET("v2/list")
     Call<List<PhotosModel>> getPhotos();
 }
